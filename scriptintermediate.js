93dcd15fba861c07c29293382023ffa5aabc6d47
Denis Ilic <ilic.denis@gmx.at>
2019-05-16 10:38:59 +0200

    Intermediate

---
+++ scriptintermediate.js
@@ -0,0 +1,9 @@
+// This sort out alphabetically
+
+var words = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+document.getElementById("words").innerHTML += words.sort();
+
+
+
+
+
