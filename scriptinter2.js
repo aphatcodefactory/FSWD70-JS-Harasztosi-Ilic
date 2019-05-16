9e761864546bec8425d22c1eb5310f4c19c00e2f
Denis Ilic <ilic.denis@gmx.at>
2019-05-16 11:11:15 +0200

    Intermediate 2

---
+++ scriptinter2.js
@@ -0,0 +1,21 @@
+//This pushes "orange" in
+var fruits = ["Apple", "banana", "kiwi"];
+fruits.push ("Orange");
+console.log(fruits);
+
+//This pulls "Orange" out again
+fruits.pop();
+console.log(fruits);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Unshift puts the "Cat" infront of "Monkey"
+var animal = ["Monkey", "Horse", "Dog"];
+animal.unshift("Cat");
+console.log(animal);
+
+animal.pop();
+console.log(animal);
+
+
+
