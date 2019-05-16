3825bb61ec94a406d5e875cb0388607159a2c119
Denis Ilic <ilic.denis@gmx.at>
2019-05-16 10:05:15 +0200

    Added variable

--- scriptbasic.js
+++ scriptbasic.js
@@ -2,3 +2,6 @@ console.log("Hello from the Console");	//This command only shows in Console
 
 document.write("Hello from the Browser");	//This command shows only in the Browser
 
+var age = 25;
+
+document.write("And i am " +age+ " years old");
